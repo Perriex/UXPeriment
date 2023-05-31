1561cd7093df3b893b33e1b2fb27dd54f0c97894
perriex <parna80as@gmail.com>
2023-05-31 22:18:00 +0330

    change brightness

--- Extenction/Code3/js/automatic/auto-adjust-brightness.js
+++ Extenction/Code3/js/automatic/auto-adjust-brightness.js
@@ -14,6 +14,6 @@ const calculate_decreasement = (precent, hours) => {
     setInterval(() => {
       initDegree -= 1;
       changeBrightness(`${initDegree}%`);
-    }, calculate_decreasement(60, 2));
+    }, calculate_decreasement(40, 2));
   });
 })();
