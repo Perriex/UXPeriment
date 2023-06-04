14f20e983d707be745511acddf5729148ac459a9
perriex <parna80as@gmail.com>
2023-06-04 14:56:38 +0330

    pre set data after update

--- Extenction/Code3/startup.js
+++ Extenction/Code3/startup.js
@@ -9,32 +9,6 @@ const is_frequency_active = () => {
   return data.click_frequency ?? [];
 };
 
-// setup features and access to local storage.
-(function start_config() {
-  console.log("%c BIPA at your service ♥! ", "background: #555; color: #4fa");
-  if (is_frequency_active() === 0) {
-    console.log(
-      "%c BIPA frequency counter is deactive.",
-      "background: #f00; color: #fa0"
-    );
-  } else {
-    console.log(
-      "%c BIPA frequency counter is activate.",
-      "background: #0f0; color: #040"
-    );
-  }
-})();
-
-const defaultObj = {
-  urls: [],
-  click_frequency: [],
-};
-
-(function setUpStorage() {
-  if (!storage.getItem(STORAGE_KEY))
-    storage.setItem(STORAGE_KEY, JSON.stringify(defaultObj));
-})();
-
 // data storage
 const get_data = () => {
   const data = JSON.parse(storage.getItem(STORAGE_KEY));
@@ -61,3 +35,52 @@ const unlock_reocord_frequency = () => {
   data.click_frequency = [];
   storage.setItem(STORAGE_KEY, JSON.stringify(data));
 };
+
+// setup features and access to local storage.
+(function start_config() {
+  console.log("%c BIPA at your service ♥! ", "background: #555; color: #4fa");
+  if (is_frequency_active() === 0) {
+    console.log(
+      "%c BIPA frequency counter is deactive.",
+      "background: #f00; color: #fa0"
+    );
+  } else {
+    console.log(
+      "%c BIPA frequency counter is activate.",
+      "background: #0f0; color: #040"
+    );
+  }
+})();
+
+const default_obj = {
+  urls: [],
+  click_frequency: [],
+  default_inputs: [],
+};
+
+const check_pre_data = (pre_data, initial) => {
+  const keys1 = Object.keys(pre_data).sort();
+  const keys2 = Object.keys(initial).sort();
+
+  const missed_keys = [];
+  for (let i = 0; i < keys1.length; i++) {
+    if (keys1[i] !== keys2[i]) {
+      missed_keys.push(keys2[i]);
+    }
+  }
+
+  return missed_keys.length > 0 ? missed_keys : false;
+};
+
+(function setUpStorage() {
+  const data = get_data();
+
+  if (!data) set_data(default_obj);
+  const missed_keys = check_pre_data(data, default_obj);
+  if (missed_keys) {
+    missed_keys.forEach((k) => {
+      data[k] = [];
+    });
+    set_data(data);
+  }
+})();
